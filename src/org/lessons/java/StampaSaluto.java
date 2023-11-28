0fd7f3b40cf320ee163a4118fe8bd4312d75d2d7
TheObscureKing <theobscureking90@gmail.com>
2023-11-28 16:09:43 +0100

    Esercizio Completato

---
+++ src/org/lessons/java/StampaSaluto.java
@@ -0,0 +1,13 @@
+package org.lessons.java;
+
+public class StampaSaluto {
+
+    public static void main(String[] args) { // scrivere main e poi TAB
+
+        // qui è dove scrivo il mio codice eseguibile.
+        System.out.println("Hello Francesco!!!"); // scrivere sout e poi TAB
+        System.out.println("Nice to meet you, I'm Francesco Barretta."); // scrivere sout e poi TAB
+
+    }
+
+}
